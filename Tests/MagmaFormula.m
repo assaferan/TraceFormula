f01fbfbf2bcd68b31e64cf11f8112dbd25b72f23
assaferan <assaferan@gmail.com>
2023-06-12 16:43:26 -0400

    added magma tests for the outputs

--- Tests/MagmaFormula.m
+++ Tests/MagmaFormula.m
@@ -237,3 +237,31 @@ end function;
 function TraceCohen(n,N,k)
   return A1(n,N,k) + A2(n,N,k) + A3(n,N,k) + A4(n,N,k);
 end function;
+
+procedure testPariVSMagma(N,k)
+    cmd := Sprintf("./src/traceALbatch_sta %o %o %o", N, N+1, k);
+    System(cmd);
+    fname := Sprintf("./data/traces_%o_%o.m", k, N);
+    r := Read(fname);
+    return_cmd := Sprintf("return traces_%o, tracesAL_%o;", N, N);
+    traces_full, traces_AL := eval(r cat return_cmd);
+    traces_magma := [TraceFormulaGamma0AL(n, N, k) : n in [1..1000]];
+    assert traces_magma eq traces_AL[2..1001];
+    return;
+end procedure;
+
+num_tests := 10;
+max_Nk2 := 10^5;
+max_level := 10000;
+Ns := [1..max_level];
+printf "testing pari vs magma implementation... (N;k) = ";
+for i in [1..num_tests] do
+    N := Random(Ns);
+    max_weight := Floor(Sqrt(max_Nk2/N));
+    ks := [2..max_weight by 2];
+    k := Random(ks);
+    printf "(%o;%o),", N, k;
+    testPariVSMagma(N,k);
+end for;
+printf "\n";
+
