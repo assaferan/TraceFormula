5e4995a2912261f49543ba049f7430917f989f0c
assaferan <assaferan@gmail.com>
2023-06-14 13:54:49 -0400

    updating the batch run script for composit levels

--- batch_run.py
+++ batch_run.py
@@ -4,29 +4,27 @@ from glob import glob
 import os
 import time
 
-from sage.all import primes
-
 k = 4
+min_N = 0
 max_N = 10000
 batch_size = 50
 num_in_command = 50
-my_primes = primes(max_N)
 fnames = glob("data/traces_*")
 covered = {int(fname.split('_')[-1].split('.')[0]) for fname in fnames}
-not_covered = sorted([x for x in my_primes if x not in covered])
+not_covered = sorted([x for x in range(min_N, max_N) if x not in covered])
 tmp = {x // num_in_command for x in not_covered}
 tmp = sorted([t for t in tmp])
 commands = ["./src/traceALbatch_sta " + str(num_in_command*i) + " " + str(num_in_command*(i + 1)) + " " + str(k) + " &" for i in tmp]
 batches = [commands[i:i+batch_size] for i in range(0,len(commands),batch_size)]
 for batch in batches:
     N_batch = int(batch[-1].split()[-2])
-    primes_tiny = {p for p in primes(N_batch)}
-    remain_tiny = sorted([x for x in primes_tiny if x not in covered])
+    levels_tiny = {N for N in range(min_N, N_batch)}
+    remain_tiny = sorted([x for x in levels_tiny if x not in covered])
     tmp = [os.system(cmd) for cmd in batch]
     while len(remain_tiny) > 0:
     	  fnames = glob("data/traces_*")
     	  covered = {int(fname.split('_')[-1].split('.')[0]) for fname in fnames}
-    	  remain_tiny = sorted([x for x in primes_tiny if x not in covered])
-    	  print(len(primes_tiny) - len(remain_tiny), "/", len(primes_tiny))
+    	  remain_tiny = sorted([x for x in levels_tiny if x not in covered])
+    	  print(len(levels_tiny) - len(remain_tiny), "/", len(levels_tiny))
     	  time.sleep(10)
     print("Finished up to ", N_batch)
