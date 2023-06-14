c1f602df766be77be1177706887cde02725f55f2
assaferan <assaferan@gmail.com>
2023-06-14 13:48:27 -0400

    modified the batch run for the general executable

--- batch_run.py
+++ batch_run.py
@@ -6,6 +6,7 @@ import time
 
 from sage.all import primes
 
+k = 4
 max_N = 10000
 batch_size = 50
 num_in_command = 50
@@ -15,7 +16,7 @@ covered = {int(fname.split('_')[-1].split('.')[0]) for fname in fnames}
 not_covered = sorted([x for x in my_primes if x not in covered])
 tmp = {x // num_in_command for x in not_covered}
 tmp = sorted([t for t in tmp])
-commands = ["./src/traceALbatch_sta " + str(num_in_command*i) + " " + str(num_in_command*(i + 1)) + " &" for i in tmp]
+commands = ["./src/traceALbatch_sta " + str(num_in_command*i) + " " + str(num_in_command*(i + 1)) + " " + str(k) + " &" for i in tmp]
 batches = [commands[i:i+batch_size] for i in range(0,len(commands),batch_size)]
 for batch in batches:
     N_batch = int(batch[-1].split()[-2])
