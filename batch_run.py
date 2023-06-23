356c4b93822538bda6aefdcf8760e7939e39d124
Eran Assaf <assaferan@gmail.com>
2023-06-23 08:07:53 -0400

    updated script for batch runs

--- batch_run.py
+++ batch_run.py
@@ -4,12 +4,12 @@ from glob import glob
 import os
 import time
 
-k = 2
+k = 4
 min_N = 1
 max_N = 100000
-batch_size = 50
-num_in_command = 50
-fnames = glob("data/traces_*")
+batch_size = 20
+num_in_command = 20
+fnames = glob("data/traces_" + str(k) + "_*")
 covered = {int(fname.split('_')[-1].split('.')[0]) for fname in fnames}
 not_covered = sorted([x for x in range(min_N, max_N) if x not in covered])
 tmp = {x // num_in_command for x in not_covered}
@@ -20,10 +20,10 @@ for batch in batches:
     N_batch = int(batch[-1].split()[-3])
     levels_tiny = {N for N in range(min_N, N_batch)}
     remain_tiny = sorted([x for x in levels_tiny if x not in covered])
-    # print("Running the following commands:", batch)
+    print("Running the following commands:", batch)
     tmp = [os.system(cmd) for cmd in batch]
     while len(remain_tiny) > 0:
-    	  fnames = glob("data/traces_*")
+    	  fnames = glob("data/traces_" + str(k) + "_*")
     	  covered = {int(fname.split('_')[-1].split('.')[0]) for fname in fnames}
     	  remain_tiny = sorted([x for x in levels_tiny if x not in covered])
     	  print(len(levels_tiny) - len(remain_tiny), "/", len(levels_tiny))
