aaa46b78bb25c8d4d02d31fc39a5a8031eed2045
Eran Assaf <assaferan@gmail.com>
2023-06-26 23:15:22 -0400

    Fixing overflow bug

--- src/traceAL_tools.h
+++ src/traceAL_tools.h
@@ -1,5 +1,9 @@
 #include <pari/pari.h>
+#include <stdint.h>
 #include <time.h>
+
+typedef uint64_t W64;
+typedef int64_t Z64;
 
 /***************************************************************/
 /*                 Generic cache handling                      */
@@ -20,14 +24,14 @@ void pari_close_mf(void);
 
 /* D > 0; 6 * hclassno(D) (6*Hurwitz). Beware, cached value for D (=0,3 mod 4)
  * is stored at D>>1 */
-ulong hclassno6u(ulong D);
+ulong hclassno6w64(W64 D);
 
 // returns 12*H
-long H12(long D);
+long H12(GEN D);
 
 GEN mksintn(long l, long x);
 
-GEN polyGegenbauer(long k, long t, long m);
+GEN polyGegenbauer(long k, GEN t, GEN m);
 
 long alpha(ulong n);
 
@@ -35,10 +39,10 @@ long alpha(ulong n);
 
 GEN traceAL(long N, long n, long k);
 
-GEN traceALprimes(long N, long k, long prec, int newspace);
+GEN traceALprimes(long N, long k, long prec, int newspace, long start);
 
-GEN trace_primes(long N, long k, long prec, int newspace);
+GEN trace_primes(long N, long k, long prec, int newspace, long start);
 
-GEN traceALupto(long N, long k, long prec);
+GEN traceALupto(long N, long k, long prec, long start);
 
-time_t timeTraceAL(long upTo, long from, long k, long num_traces, int only_primes, int newspace);
+time_t timeTraceAL(long upTo, long from, long k, long num_traces, int only_primes, int newspace, long start);
